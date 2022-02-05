bc95a46160fa224c14bcc3a79e4d38f9d3f256d6
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-02-04 20:27:44 -0500

    Add PUT, POST and DELETE actions

--- src/main/java/com/nickeecoco/restws/PatientService.java
+++ src/main/java/com/nickeecoco/restws/PatientService.java
@@ -2,11 +2,20 @@ package com.nickeecoco.restws;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import com.nickeecoco.restws.data.Patient;
 
+@Consumes("application/xml,application/json")
+@Produces("application/xml,application/json")
 @Path("/patientservice")
 public interface PatientService {
 	
@@ -14,4 +23,19 @@ public interface PatientService {
 	@GET
 	List<Patient> getPatients();
 	
+	@Path("/patients/{id}")
+	@GET
+	Patient getPatient(@PathParam("id") Long id);
+	
+	@Path("/patients")
+	@POST
+	Response createPatient(Patient patient);
+	
+	@Path("/patients")
+	@PUT
+	Response updatePatient(Patient patient);
+	
+	@Path("/patients/{id}")
+	@DELETE
+	Response deletePatient(@PathParam("id") Long id);
 }
--- src/main/java/com/nickeecoco/restws/PatientServiceImpl.java
+++ src/main/java/com/nickeecoco/restws/PatientServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.core.Response;
+
 import org.springframework.stereotype.Service;
 
 import com.nickeecoco.restws.data.Patient;
@@ -33,4 +35,48 @@ public class PatientServiceImpl implements PatientService {
 		List<Patient> response = new ArrayList<>(results);
 		return response;
 	}
+
+	@Override
+	public Patient getPatient(Long id) {
+		return patients.get(id);
+	}
+
+	@Override
+	public Response createPatient(Patient patient) {
+		patient.setId(++currentId);
+		patients.put(patient.getId(), patient);
+		return Response.ok(patient).build();
+	}
+
+	@Override
+	public Response updatePatient(Patient patient) {
+		Patient currentPatient = patients.get(patient.getId());
+		
+		Response response;
+		if(currentPatient != null) {
+			patients.put(patient.getId(), patient);
+			response = Response.ok().build();
+		} else {
+			response = Response.notModified().build();
+		}
+		
+		return response;
+	}
+
+	@Override
+	public Response deletePatient(Long id) {
+		Patient patient = patients.get(id);
+		
+		Response response;
+		if(patient != null) {
+			patients.remove(id);
+			response = Response.ok().build();
+		} else {
+			response = Response.notModified().build();
+		}
+		
+		return response;
+	}
+	
+	
 }
